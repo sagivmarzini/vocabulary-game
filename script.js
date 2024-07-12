5694ef2b13413091647861aef23c55dcfce1e9df
sagivmarzini <sagivmarzini1@gmail.com>
2024-07-12 11:54:47 +0300

    Fixed shuffleArray() function

--- script.js
+++ script.js
@@ -108,7 +108,11 @@ function handleFiles(event) {
 }
 
 function shuffleArray(array) {
-    return array.sort(() => Math.random - 0.5);
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 function processCSV(content) {
